a2cdf01466c23fd28e51a7f67410d07c648f1d43
Marius Pahl <mpahl@diw.de>
2023-04-27 19:38:08 +0200

    Adding Token in Header for redirect

--- wayf/views.py
+++ wayf/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.conf import settings
+from cryptography.fernet import Fernet
 
 def index(request):
     universities = ["Goethe-Universität", "Freie Universität Berlin", "Technische Universiät Berlin"]
@@ -17,8 +18,14 @@ def index(request):
 
 def redirect_uni(request):
     uni = request.POST.get("uni_select")
-    return redirect('/eduroam_' + uni + '/login/')
+    redir = redirect('/eduroam_' + uni + '/login/')
+    return redir
 
 @login_required(login_url='/eduroam_ffm/login/')
 def success(request):
-    return redirect(settings.AAI_REDIRECT_URL)
+    keyVar = Fernet.generate_key()
+    ferVar = Fernet(keyVar)
+    encString = ferVar.encrypt(str(request.user).encode())
+    redir = redirect(settings.AAI_REDIRECT_URL)
+    redir.headers["token"] = encString
+    return redir
