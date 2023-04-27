96d3b197ac3a8508b70e1f96a44f695b434af5c7
Klaus Illmayer <klaus.illmayer@oeaw.ac.at>
2023-04-27 13:15:55 +0200

    Adding environment variables for internal used django settings

--- aai/settings.py
+++ aai/settings.py
@@ -11,23 +11,26 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# using environs[python] for handling of environment variables
+env = Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-kmu70(g$40d%adgyvgrp4s45_s0&qcqkde0^pp#3n%qdu%=%&p"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['localhost', '127.0.0.1'])
 
 # Application definition
 
